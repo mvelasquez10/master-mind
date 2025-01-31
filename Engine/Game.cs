82d63fefbee6bb0cf4b35fc60279cb8af3b2e78d
Miguel Velasquez <vela801415@siigo.com>
2025-01-30 18:37:30 -0600

    feat: intial commit

---
+++ Engine/Game.cs
@@ -0,0 +1,151 @@
+﻿using System.Text;
+
+namespace Engine
+{
+    public class Game
+    {
+        #region Public Delegates
+
+        public delegate void GameEvent<T>(Game obj, T args) where T : EventArgs;
+
+        #endregion Public Delegates
+
+        #region Public Events
+
+        public event GameEvent<EventArgs>? OnGameFinished;
+
+        public event GameEvent<EventArgs>? OnGameStarted;
+
+        public event GameEvent<LastTryResultEventArg>? OnGameTried;
+
+        #endregion Public Events
+
+        #region Public Properties
+
+        public GameData Data { private set; get; } = new(string.Empty);
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public void Start(string secretCode, Options? options = null)
+        {
+            Data = new(secretCode, options);
+            CheckEntry(secretCode);
+            OnGameStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void TryCode(string currentTry)
+        {
+            CheckEntry(currentTry);
+            Data.LastTry = currentTry;
+            if (Data.LastTry.Equals(Data.SecrectCode))
+                FinishGame(true);
+
+            Data.LastTryResult = CalculateLastTry(Data.LastTry);
+            OnGameTried?.Invoke(this, new(Data.LastTryResult));
+
+            Data.LastTry = currentTry;
+            Data.CurrentTurn++;
+            if (Data.CurrentTurn > Data.Options.MaxTries)
+                FinishGame(false);
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private string CalculateLastTry(string lastTry)
+        {
+            var _result = new StringBuilder();
+
+            for (var i = 0; lastTry.Length > i; i++)
+            {
+                if (lastTry[i] == Data.SecrectCode[i])
+                {
+                    _result.Append("O");
+                    continue;
+                }
+                else
+                {
+                    if (Data.SecrectCode.Contains(lastTry[i]))
+                    {
+                        _result.Append("?");
+                        continue;
+                    }
+                }
+
+                _result.Append("X");
+            }
+
+            return _result.ToString();
+        }
+
+        private void CheckEntry(string entry)
+        {
+            CheckEntryDigitsAreUnique(entry);
+            CheckEntryLength(entry);
+        }
+
+        private void CheckEntryDigitsAreUnique(string entry)
+        {
+            if (entry.GroupBy(c => c).Any(g => g.Count() > 1))
+                throw new ArgumentException("The entry must contain unique characters");
+        }
+
+        private void CheckEntryLength(string entry)
+        {
+            if (entry.Length != Data.Options.CodeLength)
+                throw new ArgumentException(
+                        $"The entry must be equal to the code lenght option ({Data.Options.CodeLength})");
+        }
+
+        private void FinishGame(bool won)
+        {
+            Data.Won = won;
+            Data.Finished = DateTime.UtcNow;
+            OnGameFinished?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion Private Methods
+    }
+
+    public class LastTryResultEventArg : EventArgs
+    {
+        #region Public Constructors
+
+        public LastTryResultEventArg(string result) => Result = result;
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public string Result { init; get; }
+
+        #endregion Public Properties
+    }
+
+    public record GameData
+    {
+        public string SecrectCode { init; get; }
+        public string LastTry { set; get; } = string.Empty;
+        public string LastTryResult { set; get; } = string.Empty;
+        public ushort CurrentTurn { set; get; } = 1;
+        public DateTime? Finished { set; get; } = null;
+        public Options Options { init; get; }
+        public DateTime Started { init; get; } = DateTime.UtcNow;
+        public bool? Won { set; get; } = null;
+
+        public GameData(string secretCode, Options? options = null)
+        {
+            SecrectCode = secretCode;
+            Options = options ?? new();
+        }
+    }
+
+    public record Options
+    {
+        public uint CodeLength { init; get; } = 3;
+        public ushort MaxTries { init; get; } = 9;
+    }
+}
---
+++ TestEngine/GameTest.cs
@@ -0,0 +1,223 @@
+using Engine;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TestEngine
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Start_InvalidLength_ThrowException()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => _game.Start("1234"));
+        }
+
+        [Fact]
+        public void Start_DuplicateDigit_ThrowException()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => _game.Start("122"));
+        }
+
+        [Fact]
+        public void Start_IncressOptionCodeLength_Pass()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act & Assert
+            _game.Start("1234", new() { CodeLength = 4 });
+        }
+
+        [Fact]
+        public void Start_OnStart_RaiseStartedEvent()
+        {
+            //Arrange
+            var _game = new Game();
+            var _raised = false;
+
+            //Act
+            _game.OnGameStarted += (s, e) => _raised = true;
+            _game.Start("123");
+            
+            //Assert
+            Assert.True(_raised);
+        }
+
+        [Fact]
+        public void TryCode_AllInvalid_ReturnsXXX()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("456");
+
+            //Assert
+            Assert.Equal("XXX", _game.Data.LastTryResult);
+        }
+
+        [Fact]
+        public void TryCode_OneInvalid_ReturnsXOO()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("423");
+
+            //Assert
+            Assert.Equal("XOO", _game.Data.LastTryResult);
+        }
+
+        [Fact]
+        public void TryCode_AllPartial_ReturnsIII()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("312");
+
+            //Assert
+            Assert.Equal("???", _game.Data.LastTryResult);
+        }
+
+        [Fact]
+        public void TryCode_OnePartial_ReturnsXXI()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("345");
+
+            //Assert
+            Assert.Equal("?XX", _game.Data.LastTryResult);
+        }
+
+
+        [Fact]
+        public void TryCode_AllValid_ReturnsOOO()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("123");
+
+            //Assert
+            Assert.Equal("OOO", _game.Data.LastTryResult);
+        }
+
+        [Fact]
+        public void TryCode_OnTryCode_RaisesTriedEvent()
+        {
+            //Arrange
+            var _game = new Game();
+            var _raised = false;
+
+            //Act
+            _game.Start("123");
+            _game.OnGameTried += (s, e) => _raised = true;
+            _game.TryCode("123");
+
+            //Assert
+            Assert.True(_raised);
+        }
+
+        [Fact]
+        public void TryCode_OnTryCorrectCode_RaisesFinishEvent()
+        {
+            //Arrange
+            var _game = new Game();
+            var _raised = false;
+
+            //Act
+            _game.Start("123");
+            _game.OnGameFinished += (s, e) => _raised = true;
+            _game.TryCode("123");
+
+            //Assert
+            Assert.True(_raised);
+        }
+
+        [Fact]
+        public void TryCode_CorrectCode_WonGame()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("123");
+
+            //Assert
+            Assert.True(_game.Data.Won);
+            Assert.NotNull(_game.Data.Finished);
+        }
+
+        [Fact]
+        public void TryCode_IncorrectCode_DontWonGame()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            _game.TryCode("124");
+
+            //Assert
+            Assert.Null(_game.Data.Won);
+            Assert.Null(_game.Data.Finished);
+        }
+
+        [Fact]
+        public void TryCode_OutOfTries_FailGame()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123");
+            for (int i = 0; i < _game.Data.Options.MaxTries; i++)
+            {
+                _game.TryCode("124");
+            }
+
+            //Assert
+            Assert.False(_game.Data.Won);
+            Assert.NotNull(_game.Data.Finished);
+        }
+
+        [Fact]
+        public void TryCode_OptionLessTries_FailGame()
+        {
+            //Arrange
+            var _game = new Game();
+
+            //Act
+            _game.Start("123", new Options { MaxTries = 1 });
+            for (int i = 0; i < _game.Data.Options.MaxTries; i++)
+            {
+                _game.TryCode("124");
+            }
+
+            //Assert
+            Assert.False(_game.Data.Won);
+            Assert.NotNull(_game.Data.Finished);
+        }
+    }
+}
